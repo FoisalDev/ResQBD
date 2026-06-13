b2a0b6ef920cd8f7debf6e9b1dea3d51b90e2186
shourov <mdprincess654@gmail.com>
2026-06-13 17:17:58 +0600

    for bangladesh it will show bangla

--- client/src/utils/date.js
+++ client/src/utils/date.js
@@ -7,3 +7,8 @@ export const formatDate = (date, options = {}) => {
     ? d.toLocaleString()
     : d.toLocaleDateString();
 };
+export const toBanglaNumerals = (num) => {
+  if (num === null || num === undefined) return '';
+  const banglaDigits = ['০', '১', '২', '৩', '৪', '৫', '৬', '৭', '৮', '৯'];
+  return num.toString().replace(/\d/g, (digit) => banglaDigits[digit]);
+};
